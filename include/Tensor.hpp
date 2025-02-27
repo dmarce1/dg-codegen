23b48ce0f5538ab9d22cc1b9bf663bf054389eb1
Dominic Marcello <dmarce504@gmail.com>
2025-02-27 01:00:31 -0600

    tensor

--- include/Tensor.hpp
+++ include/Tensor.hpp
@@ -15,12 +15,9 @@ using namespace Math;
 template<typename Type, int Rank, int Ndim>
 struct Tensor;
 
-template<typename Type, int Rank, int Ndim>
-struct SymmetricTensor;
 
 template<typename Type, int Rank, int Ndim>
 struct Tensor {
-	static constexpr Type zero = Type(0.0), half = Type(0.5);
 	constexpr Tensor() = default;
 	template<typename ...Args>
 	auto& operator[](int i, Args ...args) {
@@ -123,8 +120,8 @@ struct Tensor {
 				C[i] = values[i] * other;
 			}
 		} else {
-			C = zero;
-			for (int i = 0; i < Ndim; i++) {
+			C = values[0] * other[0];
+			for (int i = 1; i < Ndim; i++) {
 				C += values[i] * other[i];
 			}
 		}
@@ -166,8 +163,8 @@ struct Tensor {
 		static constexpr int Rank1 = Rank - 2;
 		Tensor<Type, Rank1, Ndim> B;
 		if constexpr (Rank1 == 0) {
-			B = zero;
-			for (int k = 0; k < Ndim; k++) {
+			B = values[0][0];
+			for (int k = 1; k < Ndim; k++) {
 				B += values[k][k];
 			}
 		} else if constexpr (I0 > I1) {
@@ -177,8 +174,8 @@ struct Tensor {
 				B[k] = values[k].template trace<I0 - 1, I1 - 1>();
 			}
 		} else {
-			B = zero;
-			for (int k = 0; k < Ndim; k++) {
+			B = values[0].template subtensor<I1 - 1>(0);
+			for (int k = 1; k < Ndim; k++) {
 				B += values[k].template subtensor<I1 - 1>(k);
 			}
 		}
@@ -211,180 +208,15 @@ struct Tensor {
 	}
 	template<int I0 = 0, int I1 = 1>
 	auto symmetric() const {
-		return half * (*this + transpose<I0, I1>());
+		return (*this + transpose<I0, I1>());
 	}
 	template<int I0 = 0, int I1 = 1>
 	auto antisymmetric() const {
-		return half * (*this - transpose<I0, I1>());
+		return (*this - transpose<I0, I1>());
 	}
 
 private:
 	Vector<Tensor<Type, Rank - 1, Ndim>, Ndim> values;
-};
-
-template<typename Type, int Ndim>
-struct SymmetricTensor<Type, 2, Ndim> {
-	static constexpr Type zero = Type(0.0), half = Type(0.5);
-	static constexpr int Rank = 2;
-	static int const Nele = Ndim * (Ndim + 1) / 2;
-	constexpr SymmetricTensor() = default;
-	auto& operator[](int n, int m) {
-		int const i = std::max(n, m);
-		int const j = std::min(n, m);
-		return values[((i * (i + 1)) >> 1) + j];
-	}
-	auto const& operator[](int n, int m) const {
-		int const i = std::max(n, m);
-		int const j = std::min(n, m);
-		return values[((i * (i + 1)) >> 1) + j];
-	}
-	auto& operator[](Vector<int, Rank> const &indices) {
-		return operator[](indices[0], indices[1]);
-	}
-	auto const& operator[](Vector<int, Rank> const &indices) const {
-		return operator[](indices[0], indices[1]);
-	}
-	auto& operator[](int i) {
-		return values[i];
-	}
-	auto const& operator[](int i) const {
-		return values[i];
-	}
-	SymmetricTensor(SymmetricTensor const &A) :
-			values(A.values) {
-	}
-	SymmetricTensor(Type a) {
-		*this = a;
-	}
-	SymmetricTensor& operator=(SymmetricTensor const &A) {
-		values = A.values;
-		return *this;
-	}
-	SymmetricTensor& operator=(Type a) {
-		for (int i = 0; i < Nele; i++) {
-			values[i] = a;
-		}
-		return *this;
-	}
-	SymmetricTensor& operator*=(Type a) {
-		values *= a;
-		return *this;
-	}
-	SymmetricTensor& operator+=(SymmetricTensor const &A) {
-		values += A.values;
-		return *this;
-	}
-	SymmetricTensor operator-=(SymmetricTensor const &A) {
-		values -= A.values;
-		return *this;
-	}
-	SymmetricTensor operator*(Type a) {
-		SymmetricTensor B;
-		B.values = values * a;
-		return B;
-	}
-	friend SymmetricTensor operator*(Tensor<Type, 0, Ndim> a, SymmetricTensor const &B) {
-		SymmetricTensor C;
-		for (int d = 0; d < Ndim; d++) {
-			C.values[d] = a * B.values[d];
-		}
-		return C;
-	}
-	SymmetricTensor operator+(SymmetricTensor const &A) const {
-		SymmetricTensor B;
-		B.values = values + A.values;
-		return B;
-	}
-	SymmetricTensor operator-(SymmetricTensor const &A) const {
-		SymmetricTensor B;
-		for (int d = 0; d < Ndim; d++) {
-			B.values[d] = values[d] - A.values[d];
-		}
-		return B;
-	}
-	SymmetricTensor operator+() const {
-		return *this;
-	}
-	SymmetricTensor operator-() const {
-		SymmetricTensor A;
-		for (int d = 0; d < Ndim; d++) {
-			A.values[d] = -values[d];
-		}
-		return A;
-	}
-	auto operator*(SymmetricTensor const &B) const {
-		auto const &A = *this;
-		Tensor<Type, Rank, Ndim> C;
-		Real const ab00 = A.values[0] * B.values[0];
-		Real const ab11 = A.values[1] * B.values[1];
-		Real const ab22 = A.values[2] * B.values[2];
-		Real const ab01 = A.values[0] * B.values[1];
-		Real const ab13 = A.values[1] * B.values[3];
-		Real const ab24 = A.values[2] * B.values[4];
-		Real const ab03 = A.values[0] * B.values[3];
-		Real const ab14 = A.values[1] * B.values[4];
-		Real const ab25 = A.values[2] * B.values[5];
-		Real const ab10 = A.values[1] * B.values[0];
-		Real const ab31 = A.values[3] * B.values[1];
-		Real const ab42 = A.values[4] * B.values[2];
-		Real const ab33 = A.values[3] * B.values[3];
-		Real const ab44 = A.values[4] * B.values[4];
-		Real const ab12 = A.values[1] * B.values[2];
-		Real const ab34 = A.values[3] * B.values[4];
-		Real const ab45 = A.values[4] * B.values[5];
-		Real const ab20 = A.values[2] * B.values[0];
-		Real const ab41 = A.values[4] * B.values[1];
-		Real const ab52 = A.values[5] * B.values[2];
-		Real const ab21 = A.values[2] * B.values[1];
-		Real const ab43 = A.values[4] * B.values[3];
-		Real const ab54 = A.values[5] * B.values[4];
-		Real const ab55 = A.values[5] * B.values[5];
-		C[0, 0] = ab00 + ab11 + ab22;
-		C[0, 1] = ab01 + ab13 + ab24;
-		C[0, 2] = ab03 + ab14 + ab25;
-		C[1, 0] = ab10 + ab31 + ab42;
-		C[1, 1] = ab11 + ab33 + ab44;
-		C[1, 2] = ab12 + ab34 + ab45;
-		C[2, 0] = ab20 + ab41 + ab52;
-		C[2, 1] = ab21 + ab43 + ab54;
-		C[2, 2] = ab22 + ab44 + ab55;
-		return C;
-	}
-	template<int Rank1>
-	auto operator^(Tensor<Type, Rank1, Ndim> const &A) const {
-		static constexpr int Rank2 = Rank + Rank1;
-		Tensor<Type, Rank2, Ndim> B;
-		if constexpr (Rank > 1) {
-			for (int i = 0; i < Ndim; i++) {
-				B[i] = values[i] ^ A;
-			}
-		} else {
-			for (int i = 0; i < Ndim; i++) {
-				for (int j = 0; j < Ndim; j++) {
-					B[i, j] = values[i] * A[j];
-				}
-			}
-		}
-		return B;
-	}
-	Type trace() const {
-		return values[0] + values[3] + values[5];
-	}
-
-	SymmetricTensor transpose() const {
-		return *this;
-	}
-
-	auto symmetric() const {
-		return *this;
-	}
-
-	auto antisymmetric() const {
-		return Tensor<Type, Rank, Ndim>(zero);
-	}
-
-private:
-	Vector<Type, Nele> values;
 };
 
 template<typename Type, int Ndim>
@@ -441,7 +273,7 @@ struct Tensor<Type, 0, Ndim> {
 	Tensor operator/(Tensor a) const {
 		return Tensor(value / a.value);
 	}
-	explicit operator Type() const {
+	operator Type() const {
 		return value;
 	}
 	friend Tensor operator+(Type a, Tensor b) {
@@ -505,25 +337,9 @@ auto tensorInverse(Tensor<Type, 2, Ndim> const &A) {
 		}
 	}
 	for (int i = 0; i < Ndim; i++) {
-		det += Real(A[0, i] * Ainv[i, 0]);
+		det += Type(A[0, i] * Ainv[i, 0]);
 	}
 	Ainv = (one / det) * Ainv;
-	return Ainv;
-}
-
-template<typename Type, int Ndim>
-auto tensorInverse(SymmetricTensor<Type, 2, Ndim> const &A) {
-	SymmetricTensor<Type, 2, Ndim> Ainv;
-	Ainv[0, 0] = A[1, 1] * A[2, 2] - A[1, 2] * A[1, 2];
-	Ainv[0, 1] = A[0, 2] * A[1, 2] - A[0, 1] * A[2, 2];
-	Ainv[0, 2] = A[0, 1] * A[1, 2] - A[0, 2] * A[1, 1];
-	auto const detinv = Type(1) / (A[0, 0] * Ainv[0, 0] + A[0, 1] * Ainv[0, 1] + A[0, 2] * Ainv[0, 2]);
-	Ainv[0, 0] *= detinv;
-	Ainv[0, 1] *= detinv;
-	Ainv[0, 2] *= detinv;
-	Ainv[1, 1] = (A[0, 0] * A[2, 2] - A[0, 2] * A[0, 2]) * detinv;
-	Ainv[1, 2] = (A[0, 2] * A[0, 1] - A[0, 0] * A[1, 2]) * detinv;
-	Ainv[2, 2] = (A[0, 0] * A[1, 1] - A[0, 1] * A[0, 1]) * detinv;
 	return Ainv;
 }
 
--- src/Einstein.cpp
+++ src/Einstein.cpp
@@ -5,300 +5,200 @@
  *      Author: dmarce1
  */
 
-#include <algorithm>
-#include <array>
-#include <numeric>
-#include <tuple>
-#include <stdio.h>
-#include <tuple>
-#include <string>
-#include <utility>
-
-#include <utility>
-#include <type_traits>
-
-template<int I1, int ...I2>
-struct Symmetry {
-	constexpr auto operator()(std::integer_sequence<int, I1, I2...> = std::integer_sequence<int, I1, I2...>()) {
-		static constexpr int size = 1 + sizeof...(I2);
-		std::array<int, size> indices;
-		indices[0] = I1;
-		if constexpr (size > 1) {
-			Symmetry<I2...> nextFunctor;
-			auto const theRest = nextFunctor();
-			static_assert(theRest.size() == size - 1);
-			for (size_t i = 0; i < theRest.size(); i++) {
-				indices[i + 1] = theRest[i];
-			}
-		}
-		return indices;
-	}
-	template<int J, int K>
-	using contracted_type = Symmetry< I1 - int(I1>J) - int(I1>K), (I2 - int(I2>J) - int(I2>K))... >;
-};
-
-template<int I, typename Seq>
-struct contains;
-
-template<int I, int Head, int ... Tail>
-struct contains<I, Symmetry<Head, Tail...> > {
-	static constexpr bool value = ((I == Head) ? true : contains<I, Symmetry<Tail...>>::value);
-};
-
-template<int I>
-struct contains<I, void> {
-	static constexpr bool value = false;
-};
-
-template<int I, int ... Is>
-struct symmetryContract {
-	using type = Symmetry< ((Is > I) ? (Is - 1) : Is)... >;
-};
-
-template<int RANK, int DIM>
-static constexpr int indices2Index(std::array<int, RANK> indices) {
-	return std::accumulate(indices.rbegin(), indices.rend(), 0, [](int i, int j) {
-		return DIM * i + j;
-	});
-}
-
-template<int RANK, int DIM>
-static constexpr std::array<int, RANK> index2Indices(int index) {
-	std::array<int, RANK> indices;
-	for (int i = 0; i < RANK; i++) {
-		indices[i] = index % DIM;
-		index /= DIM;
-	}
-	return indices;
-}
+#include "Vector.hpp"
 
 constexpr int pow(int x, int n) {
-	int xm = x;
-	int xn = 1;
-	while (n > 0) {
-		if (n & 1) {
-			xn *= xm;
-		}
-		xm *= xm;
-		n >>= 1;
+	int y = 1;
+	for (int m = 0; m < n; m++) {
+		y *= x;
 	}
-	return xn;
+	return y;
 }
 
-template<typename ...ARGS>
-struct FirstArgument;
-
-template<>
-struct FirstArgument<> {
-	using type = void;
+enum class symmetry_t : int {
+	asymmetric, symmetric, antisymmetric
 };
 
-template<typename ... ARGS>
-constexpr std::array<int, sizeof...(ARGS)> ints2Array(ARGS ... args) {
-	static_assert((std::is_integral_v<ARGS> && ...), "All arguments must be integers.");
-	return {static_cast<int>(args)...};
-}
-
-template<typename FIRST, typename ...ARGS>
-struct FirstArgument<FIRST, ARGS...> {
-	using type = FIRST;
-};
-
-template<int RANK, typename FIRST = void, typename ...ARGS>
-static constexpr auto sortIndices(std::array<int, RANK> const &indices) {
-	if constexpr (std::is_void<FIRST>::value) {
-		return indices;
-	} else {
-		FIRST firstSymmetry;
-		auto const symmetrySet = firstSymmetry();
-		auto sortedSymmetries = symmetrySet;
-		auto sortedIndices = indices;
-		std::sort(sortedSymmetries.begin(), sortedSymmetries.end(), [indices](int a, int b) {
-			return indices[a] > indices[b];
-		});
-		for (size_t i = 0; i < sortedSymmetries.size(); i++) {
-			sortedIndices[symmetrySet[i]] = indices[sortedSymmetries[i]];
+template<int R, int D>
+auto symmetricMapping() {
+	std::array<int, R> indices;
+	std::array<int, pow(D, R)> map;
+	std::fill(indices.begin(), indices.end(), 0);
+	std::fill(map.begin(), map.end(), -1);
+	auto const size = pow(D, R);
+	int next = 0;
+	for (int l = 0; l < size; l++) {
+		auto common = indices;
+		std::sort(common.begin(), common.end());
+		int k = 0;
+		for (int d = 0; d < R; d++) {
+			k = D * k + common[d];
 		}
-		if constexpr (sizeof...(ARGS)) {
-			return sortIndices<RANK, ARGS...>(sortedIndices);
-		} else {
-			return sortedIndices;
+		if (map[k] < 0) {
+			map[k] = next++;
+		}
+		map[l] = map[k];
+		int dim = 0;
+		while ((++indices[dim] == D) && (dim < R)) {
+			indices[dim++] = 0;
+		}
+	}
+	std::fill(indices.begin(), indices.end(), 0);
+	for (int l = 0; l < size; l++) {
+		int dim = 0;
+		while ((++indices[dim] == D) && (dim < R)) {
+			indices[dim++] = 0;
 		}
 	}
 }
 
-template<int RANK, int DIM, typename ...ARGS>
-static constexpr auto createIndexMap() {
-	static constexpr int size = pow(DIM, RANK);
-	std::array<int, size> map;
-	std::array<bool, size> filled;
-	std::fill(filled.begin(), filled.end(), false);
-	int nextIndex = 0;
-	for (int thisIndex = 0; thisIndex < size; thisIndex++) {
-		auto const theseIndices = index2Indices<RANK, DIM>(thisIndex);
-		auto const commonIndices = sortIndices<RANK, ARGS...>(theseIndices);
-		int const commonIndex = indices2Index<RANK, DIM>(commonIndices);
-		if (!filled[commonIndex]) {
-			map[commonIndex] = nextIndex++;
-			filled[thisIndex] = true;
-		}
-		map[thisIndex] = map[commonIndex];
-	}
-	return map;
-}
+template<typename T, int R, int D, symmetry_t SYMMETRY = symmetry_t::asymmetric>
+class Tensor;
 
-template<int I1, int I2, typename Head, typename ... Tail>
-struct SymmetryContraction {
-	using head_type = std::conditional<
-	contains<I1, Head, Tail...>::value || contains<I2, Head, Tail...>::value,
-	std::tuple<>,
-	std::tuple<typename Head::contracted_type<I1, I2>>
-	>::type;
-	using tail_type = SymmetryContraction<I1, I2, Tail...>::type;
-	using type = std::remove_reference<decltype(std::tuple_cat(head_type(), tail_type()))>::type;
-};
+template<typename DERIVED, typename T, int S, int R, int D>
+struct TensorBase: public Math::Vector<T, S> {
+	using REAL = T;
+	static constexpr int SIZE = S;
+	static constexpr int RANK = R;
+	static constexpr int DIMS = D;
+	using base_type = Math::Vector<REAL, SIZE>;
 
-template<typename DERIVED, int RANK, int DIM, typename ...SYMMETRIES>
-struct TensorBase {
-
-	template<typename ...ARGS>
-	constexpr auto const& operator[](ARGS &&... args) const {
-		return derived.access_impl(std::forward<ARGS>(args)...);
+	REAL const& operator[](auto ...i) const {
+		return static_cast<DERIVED*>(this)->access(i...);
 	}
 
-	template<typename ...ARGS>
-	constexpr auto& operator[](ARGS &&... args) {
-		return derived.assign_impl(std::forward<ARGS>(args)...);
+	REAL& operator[](auto ...i) {
+		return static_cast<DERIVED*>(this)->assign(i...);
 	}
 
-private:
-	DERIVED &derived = *static_cast<DERIVED*>(this);
-};
-
-template<typename, int, int, typename ...>
-struct contracted_type;
-
-//template<int I1, int I2, int RANK, int DIM>
-//static constexpr auto contractionList() {
-//	static constexpr int SIZE = pow(DIM, RANK - 2);
-//	std::array<std::array<int, DIM>, SIZE> lists;
-//	int next_index = 0;
-//	for (int k = 0; k < RANK * SIZE; k++) {
-//		auto const theseIndices = index2Indices<RANK, DIM>(k);
-//		if (theseIndices[I1] = theseIndices[I2]) {
-//			for (int l = 0; l < DIM; l++) {
-//				lists[next_index++][theseIndices[I1]] = k;
-//			}
-//		}
-//	}
-//	return lists;
-//}
-
-template<typename TYPE, int RANK, int DIM, typename ...SYMMETRIES>
-struct Tensor: public TensorBase<Tensor<TYPE, RANK, DIM, SYMMETRIES...>, RANK, DIM, SYMMETRIES...> {
-	using value_type = TYPE;
-	static constexpr int rank = RANK;
-	static constexpr int ndim = DIM;
-
-	template<int I1 = 0, int I2 = 1>
-	constexpr auto contract() const {
-		using sym_tuple = SymmetryContraction<I1, I2, SYMMETRIES...>::type;
-		using return_type = contracted_type<TYPE, RANK, DIM, sym_tuple>::type;
-		return_type result;
-
-		return result;
-	}
-
-	constexpr auto const& access_impl(std::array<int, RANK> const &indices) const {
-		return values[indexMap[indices2Index<rank, ndim>(indices)]];
-	}
-
-	constexpr auto& access_impl(std::array<int, RANK> const &indices) {
-		return values[indexMap[indices2Index<rank, ndim>(indices)]];
-	}
-
-	template<typename ...ARGS>
-	constexpr auto const& access_impl(int I, ARGS &&... args) const {
-		return access_impl(ints2Array(I, std::forward<ARGS>(args)...));
-	}
-
-	template<typename ...ARGS>
-	auto& assign_impl(int I, ARGS &&... args) {
-		return access_impl(ints2Array(I, std::forward<ARGS>(args)...));
-	}
-
-private:
-	static constexpr auto indexMap = createIndexMap<rank, ndim, SYMMETRIES...>();
-	static constexpr int size = *std::max_element(indexMap.begin(), indexMap.end()) + 1;
-	std::array<TYPE, size> values;
-};
-
-template<typename TYPE, int RANK, int DIM>
-struct Tensor<TYPE, RANK, DIM> : public TensorBase<Tensor<TYPE, RANK, DIM>, RANK, DIM> {
-	using value_type = TYPE;
-	static constexpr int rank = RANK;
-	static constexpr int ndim = DIM;
-
-	template<int I1 = 0, int I2 = 1>
-	constexpr auto contract() const {
-		if constexpr (I1 > I2) {
-			return contract<I2, I1>();
-		} else {
-			Tensor<TYPE, RANK - 2, DIM> result;
-			std::array<int, RANK> Is;
-			std::fill(Is.begin(), Is.end(), 0);
-			constexpr int Imin = std::min(I1, I2);
-			constexpr int Imax = std::max(I1, I2);
-			constexpr int N1 = pow(DIM, Imin);
-			constexpr int N2 = pow(DIM, Imax - Imin);
-			constexpr int N3 = pow(DIM, RANK - Imax);
-			for (int n3 = 0; n3 < N3; n3++) {
-				for (int n2 = 0; n2 < N2; n2++) {
-					for (int n1 = 0; n1 < N1; n1++) {
-						TYPE sum = TYPE(0);
-						int const index1 = N1 * (N2 * n3 + n2) + n1;
-						int const index2 = N1 * DIM * (N2 * DIM * n3 + n2) + n1;
-						int const index3 = N1 * (DIM * N2 + 1);
-						for (int k = 0; k < DIM; k++) {
-						}
+	template<int I0 = 0, int I1 = 1>
+	Tensor<T, R - 2, D> trace() const {
+		Tensor<T, R - 2, D> result;
+		static constexpr int J0 = std::min(I0, I1);
+		static constexpr int J1 = std::max(I0, I1);
+		static constexpr int N1 = pow(D, J0);
+		static constexpr int N2 = pow(D, J1 - J0);
+		static constexpr int N3 = pow(D, R - J1);
+		static constexpr int DK = N3 * (D * N2 + 1);
+		for (int n1 = 0; n1 < N1; n1++) {
+			for (int n2 = 0; n2 < N2; n2++) {
+				for (int n3 = 0; n3 < N3; n3++) {
+					int const index1 = n3 + N3 * (n2 + N2 * n1);
+					int const index2 = n3 + N3 * D * (n2 + N2 * D * n1);
+					auto &res = (Math::Vector<T, pow(D, R - 2)>&) result[index1];
+					auto const &src = (Math::Vector<T, S>&) (*this);
+					res = REAL(0);
+					for (int k = 0; k < D; k++) {
+						res += src[index2 + DK * k];
 					}
 				}
 			}
-			return result;
 		}
+		return result;
 	}
-
-	constexpr auto const& access_impl(std::array<int, RANK> const &indices) const {
-		return values[indices2Index<rank, ndim>(indices)];
-	}
-
-	constexpr auto access_impl(std::array<int, RANK> const &indices) {
-		return values[indices2Index<rank, ndim>(indices)];
-	}
-
-	template<typename ...ARGS>
-	constexpr auto const& access_impl(int I, ARGS &&... args) const {
-		return access_impl(ints2Array(I, std::forward<ARGS>(args)...));
-	}
-
-	template<typename ...ARGS>
-	constexpr auto& assign_impl(int I, ARGS &&... args) {
-		return access_impl(ints2Array(I, std::forward<ARGS>(args)...));
-	}
-
-private:
-	static constexpr int size = pow(ndim, rank);
-	std::array<TYPE, size> values;
 };
 
-template<typename TYPE, int RANK, int DIM, typename ...SYMMETRIES>
-struct contracted_type<TYPE, RANK, DIM, std::tuple<SYMMETRIES...>> {
-	using type = Tensor<TYPE, RANK - 2, DIM, SYMMETRIES...>;
+template<typename T, int R, int D>
+class TensorDelta: public TensorBase<TensorDelta<T, R, D>, T, 0, R, D> {
+	T const& access(int i, int j) const {
+		return (i == j);
+	}
+};
+
+template<typename T, int R, int D, symmetry_t SYMMETRY>
+class Tensor: public TensorBase<Tensor<T, R, D, SYMMETRY>, T, Tensor<T, R, D, SYMMETRY>::size(), R, D> {
+	static constexpr int size() {
+		if constexpr (SYMMETRY == symmetry_t::asymmetric) {
+			int n = 1;
+			int d = 1;
+			for (int r = 0; r < R; r++) {
+				n *= D + r;
+				d *= r + 1;
+			}
+			return n / d;
+		} else if constexpr (SYMMETRY == symmetry_t::antisymmetric) {
+			int n = 1;
+			int d = 1;
+			for (int r = 0; r < R; r++) {
+				n *= D - r;
+				d *= r + 1;
+			}
+			return n / d;
+		} else if constexpr (SYMMETRY == symmetry_t::symmetric) {
+			return pow(D, R);
+		}
+	}
+	;
+	using base_type = TensorBase<Tensor, T, size(), R, D>;
+	base_type::base_type &data;
+	Tensor() :
+			data(*this) {
+	}
+	int findIndex(int sum, int i0, auto ...i1) const {
+		if constexpr (SYMMETRY == symmetry_t::asymmetric) {
+			auto constexpr static map = symmetricMapping<R, D>();
+			return findIndex(D * sum + i0, i1...);
+		} else if constexpr (SYMMETRY == symmetry_t::symmetric) {
+			return findIndex(D * sum + i0, i1...);
+		}
+	}
+	T const& access(auto ...i) const {
+		return data[findIndex(0, i...)];
+	}
+	T& assign(auto ...i) {
+		return data[findIndex(0, i...)];
+	}
+	friend base_type;
+};
+
+template<typename TENSOR, int I1 = 0, int I2 = 1>
+struct TensorTranspose: public TensorBase<TensorTranspose<TENSOR, I1, I2>, typename TENSOR::REAL, 0, TENSOR::RANK, TENSOR::DIMS> {
+	using REAL = typename TENSOR::REAL;
+	static constexpr int RANK = TENSOR::RANK;
+	static constexpr int DIMS = TENSOR::DIMS;
+	TensorTranspose(TENSOR const &t1) :
+			T1(t1) {
+	}
+	REAL const& access(auto ...index) const {
+		std::array<int, RANK> pack;
+		int i = 0;
+		((pack[i++] = index),...);
+		std::swap(pack[I1], pack[I2]);
+		return std::apply([this](auto ...i) {
+			return T1(i...);
+		}, pack);
+	}
+private:
+	TENSOR const &T1;
+
+};
+
+template<typename TENSOR1, typename TENSOR2>
+struct TensorProduct: public TensorBase<TensorProduct<TENSOR1, TENSOR2>, typename TENSOR1::REAL, 0, (TENSOR1::RANK + TENSOR2::RANK), TENSOR1::DIMS> {
+	static constexpr int RANK1 = TENSOR1::RANK;
+	static constexpr int RANK2 = TENSOR2::RANK;
+	static constexpr int RANK = RANK1 + RANK2;
+	static constexpr int DIMS = TENSOR1::DIMS;
+	using REAL = typename TENSOR1::REAL;
+	TensorProduct(TENSOR1 const &t1, TENSOR2 const &t2) :
+			T1(t1), T2(t2) {
+	}
+	REAL const& access(auto ...index) const {
+		std::array<int, RANK1> pack1;
+		std::array<int, RANK2> pack2;
+		int i = 0;
+		((((i < RANK1) ? &pack1[0] : &pack2[-RANK1])[i++] = index),...);
+		return std::apply([this](auto ...i) {
+			return T1(i...);
+		}, pack1) * std::apply([this](auto ...i) {
+			return T2(i...);
+		}, pack2);
+	}
+private:
+	TENSOR1 const &T1;
+	TENSOR2 const &T2;
 };
 
 void testEinstein() {
-	constexpr int RANK = 4;
-	constexpr int DIM = 3;
-	Tensor<double, RANK, DIM, Symmetry<1, 2>> Ta;
-	Ta[2, 3, 4, 5];
+	symmetricMapping<2, 3>();
 }
