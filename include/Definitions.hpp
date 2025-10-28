3d94096463ce22e8e0f9c3b655d232d15da0c94b
Dominic Marcello <dmarce504@gmail.com>
2025-10-28 13:20:26 -0500

    unused removed

--- include/Definitions.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-//static constexpr auto maximumSimdWidth = SIMD_WIDTH;
-//static constexpr auto dimensionCount = DIMENSION_COUNT;
-//static constexpr auto modeCount = MODE_COUNT;
-//
